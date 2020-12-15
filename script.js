b9978447801ffbc3ac75adaa1db52a5c59af3db2
raych2 <rtongco@gmail.com>
2020-12-14 22:02:23 -0800

    Add player factory and initial game module

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const gameBoard = (() => {
         container.style.gridTemplateColumns = `repeat(3, 1fr)`;
         for(let i = 0; i < gameBoardArr.length; i++) {
             let square = document.createElement('div');
-            square.innerHTML = 'X';
-            square.classList.add('blank');
+            square.dataset.order = i;
+            square.classList.add('empty');
             container.append(square);
         }
     }
@@ -21,3 +21,37 @@ const gameBoard = (() => {
 })();
 
 gameBoard.renderGameBoard();
+
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+    return {name, mark};
+}
+
+const playerOne = Player('playerOne', 'X');
+const playerTwo = Player('playerTwo', 'O');
+
+const game = (() => {
+    const squares = document.querySelectorAll('.empty');
+    const board = gameBoard.gameBoardArr;
+    let currentPlayer = playerOne;
+    let winner;
+
+    const play = (e) => {
+        if(currentPlayer === null) {
+            currentPlayer = playerOne;
+        } else if (currentPlayer === playerOne) {
+            board[`${e.target.dataset.order}`] = playerOne.mark;
+            e.target.removeEventListener('click', play);
+            currentPlayer = playerTwo;
+        } else {
+            board[`${e.target.dataset.order}`] = playerTwo.mark;
+            e.target.removeEventListener('click', play);
+            currentPlayer = playerOne;
+        }
+    }
+
+    squares.forEach(square => {
+        square.addEventListener('click', play);
+    });
+})();

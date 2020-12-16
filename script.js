e3ada64933b8e60a9582e198f2db70e761d831db
raych2 <rtongco@gmail.com>
2020-12-16 15:17:22 -0800

    Add functions to find winner and end game if winner is determined

--- script.js
+++ script.js
@@ -33,24 +33,73 @@ const playerTwo = Player('playerTwo', 'O');
 
 const game = (() => {
     const squares = document.querySelectorAll('.empty');
+    const gameWinner = document.querySelector('.winner');
     const board = gameBoard.gameBoardArr;
-    let currentPlayer = playerOne;
+    let currentPlayer;
     let winner;
 
     const play = (e) => {
         if(currentPlayer === null) {
             currentPlayer = playerOne;
-        } else if (currentPlayer === playerOne) {
-            board[`${e.target.id}`] = playerOne.mark;
-            displayController.displayMarkers();
-            e.target.removeEventListener('click', play);
-            currentPlayer = playerTwo;
-        } else {
+        } else if (currentPlayer === playerTwo) {
             board[`${e.target.id}`] = playerTwo.mark;
             displayController.displayMarkers();
             e.target.removeEventListener('click', play);
+            findWinner();
             currentPlayer = playerOne;
+        } else {
+            currentPlayer = playerOne;
+            board[`${e.target.id}`] = playerOne.mark;
+            displayController.displayMarkers();
+            e.target.removeEventListener('click', play);
+            findWinner();
+            currentPlayer = playerTwo;
         }
+    }
+
+    const findWinner = () => {
+        if(getHorizontalWin() || getVerticalWin() || getDiagonalWin()) {
+            endGame();
+            gameWinner.innerText = `The winner is ${winner.name}! Play again!`
+        }
+    }
+
+    const determineWin = (firstSpace, secondSpace, thirdSpace) => {
+        if(firstSpace === secondSpace && secondSpace === thirdSpace) {
+            if(firstSpace === 'X') {
+                currentPlayer = playerOne;
+                winner = currentPlayer;
+            } else if(firstSpace === 'O') {
+                currentPlayer = playerTwo;
+                winner = currentPlayer;
+            }
+        }
+        return winner;
+    }
+    const getHorizontalWin = () => {
+        let firstRow = determineWin(board[0], board[1], board[2]);
+        let secondRow = determineWin(board[3], board[4], board[5]);
+        let thirdRow = determineWin(board[6], board[7], board[8]);
+        return firstRow || secondRow || thirdRow;
+    }
+
+    const getVerticalWin = () => {
+        let firstCol = determineWin(board[0], board[3], board[6]);
+        let secondCol = determineWin(board[1], board[4], board[7]);
+        let thirdCol = determineWin(board[2], board[5], board[8]);
+        return firstCol || secondCol || thirdCol;
+    }
+
+    const getDiagonalWin = () => {
+        let firstDiag = determineWin(board[0], board[4], board[8]);
+        let secondDiag = determineWin(board[2], board[4], board[6]);
+        return firstDiag || secondDiag;
+    }
+
+    const endGame = () => {
+        squares.forEach(square => {
+            square.removeEventListener('click', play);
+        });
     }
 
     squares.forEach(square => {
@@ -70,4 +119,3 @@ const displayController = (() => {
 
     return {displayMarkers}
 })();
-
